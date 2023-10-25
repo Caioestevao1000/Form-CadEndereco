4ac01fa31b41ba89fabf1cd7781de661266504cf
Caio Estevão <108750017+Caioestevao1000@users.noreply.github.com>
2023-10-25 20:21:14 -0300

    add https

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ const preencherFormulario = (endereco) => {
 //  Função para consumo de APi da VIA CEP
 const pesquisarCEP = async() => {
     limparFomulario();
-    const url = `http://viacep.com.br/ws/${cep.value}/json/`;
+    const url = `https://viacep.com.br/ws/${cep.value}/json/`;
 
     if(cepValido(cep.value)){
         const dados = await fetch(url); // esperar fetch liga a API
